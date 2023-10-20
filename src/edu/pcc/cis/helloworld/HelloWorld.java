2b53ba68679295fe872fe16d0c6c61cf849c8312
scheabe <schea.bowden@pcc.edu>
2023-10-20 15:03:51 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,8 @@
 package edu.pcc.cis.helloworld;
 
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello, World!");
     }
 }
